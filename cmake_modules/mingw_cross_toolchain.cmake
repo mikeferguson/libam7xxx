83413d14e30727d4279674d5bb811f436c9600f2
Antonio Ospite <ospite@studenti.unina.it>
2012-05-22 15:29:55 +0200

    mingw_cross_toolchain.cmake: set the MINGW variable to True
    Setting MINGW to True is important to let cmake set some variables such
    as CMAKE_FIND_LIBRARY_PREFIXES and CMAKE_FIND_LIBRARY_SUFFIXES from
    Platform/Windows-GNU.cmake
    
    I wasted a lot of time on this with find_library() not finding libav
    libraries compiled for MingW whith the ".dll.a" suffix.

--- cmake_modules/mingw_cross_toolchain.cmake
+++ cmake_modules/mingw_cross_toolchain.cmake
@@ -7,3 +7,8 @@ ENDIF()
 CMAKE_FORCE_C_COMPILER(${GNU_HOST}-gcc GNU)
 # CMake doesn't automatically look for prefixed 'windres', do it manually:
 SET(CMAKE_RC_COMPILER ${GNU_HOST}-windres)
+
+# The following is important to let cmake set some variables such as
+# CMAKE_FIND_LIBRARY_PREFIXES and CMAKE_FIND_LIBRARY_SUFFIXES from
+# Platform/Windows-GNU.cmake
+SET(MINGW True)
